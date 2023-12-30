398089a4a3f9d8d1245e5f6b1d090182c8d214b4
TaichiYazaki <taichi.yazaki@rakus-partners.co.jp>
2023-12-30 05:07:33 +0000

    add source main.py

--- main.py
+++ main.py
@@ -5,14 +5,21 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.select import Select
 from selenium import webdriver
 
-# カートのURLを貼り付けてください。
-BASE_URL = "https://jp.supreme.com/checkouts/c/f9b3f21e2fc69cc57cfe7ac836843c9b?ew_m=f"
+BASE_URL = "https://jp.supreme.com/products/2i_0eglt08q5z-te"
+
 options = webdriver.ChromeOptions()
 driver = webdriver.Remote(
         command_executor=os.environ["SELENIUM_URL"],
         options = webdriver.ChromeOptions()
 )
 driver.get(BASE_URL)
+select_size = driver.find_element(By.XPATH, "/html/body/div[1]/div[2]/main/div[3]/div/div/div/div/div[2]/div[2]/form/div[2]/div/div[4]/div[2]/div[2]/div[1]/div[2]/div/select")
+size = Select(select_size)
+size.select_by_value("42725425381580")
+driver.find_element(By.XPATH, "/html/body/div[1]/div[2]/main/div[3]/div/div/div/div/div[2]/div[2]/form/div[2]/div/div[4]/div[2]/div[2]/div[3]/div[1]/input").click()
+time.sleep(1)
+choose_payment_page = driver.find_element(By.XPATH, "/html/body/div[1]/div[2]/main/div[3]/div/div/div/div/div[1]/div/div/div/a[2]")
+driver.execute_script('return arguments[0].click()', choose_payment_page)
 
 load_dotenv()
 EMAIL = os.getenv('EMAIL')
@@ -37,7 +44,8 @@ prefecture.select_by_value("JP-11")
 
 select_payment_method = driver.find_element(By.XPATH, "/html/body/div[1]/div/div/div/div[1]/div[1]/div/div[2]/div/div[1]/div/div[1]/div/main/div[1]/form/div[1]/div/div/div[3]/div/section/div/div[2]/div/div/div/div/fieldset/div/div[2]/label/div/div[1]/input")
 driver.execute_script('return arguments[0].click()', select_payment_method)
-
-# 買わない時は押さないこと
-#driver.find_element(By.XPATH, "/html/body/div[1]/div/div/div/div[1]/div[1]/div/div[2]/div/div[1]/div/div[1]/div/main/div/form/div[1]/div/div/div[4]/div/section/div/div/div/div/div/div/div/button").click()
+time.sleep(1)
+# # 買わない時は押さないこと
+# #driver.find_element(By.XPATH, "/html/body/div[1]/div/div/div/div[1]/div[1]/div/div[2]/div/div[1]/div/div[1]/div/main/div/form/div[1]/div/div/div[4]/div/section/div/div/div/div/div/div/div/button").click()
+time.sleep(5)
 driver.quit()
